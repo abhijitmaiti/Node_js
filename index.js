49a742bfeee53d9f2c4351dc911de2b83eea0e6c
Abhijit Maiti <72270611+abhijitmaiti@users.noreply.github.com>
2023-11-06 00:49:48 +0530

    create first server

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-// const http = require('http');
-// const data= require('./data.js');
-// const { json } = require('stream/consumers');
-// http.createServer((req, rep) => {
-//     rep.writeHead(200,{'content-type':'application/json'});
-//     rep.write(JSON.stringify(data));
-//     rep.end();
-
-// }).listen(8080);
-
-
-// const fs = require('fs');
-
-// const input = process.argv;
-// if(input[2]=='add'){
-//     fs.writeFileSync(input[3],input[4]);
-// }
-// else if(input[2]=='remove'){
-//     fs.unlinkSync(input[3]);
-// }
-// else
-//     console.log("Invalid output");
-
-
-const dataSet = require('./data.js');
 const http = require('http');
-http.createServer((req,rep)=>{
-    rep.writeHead(200,{'content-type':'application/Json'});
-    rep.write(JSON.stringify(dataSet));
+const data= require('./data.js');
+const { json } = require('stream/consumers');
+http.createServer((req, rep) => {
+    rep.writeHead(200,{'content-type':'application/json'});
+    rep.write(JSON.stringify(data));
     rep.end();
 
-}).listen(3000);
+}).listen(8080);
+
+
+const fs = require('fs');
+
+const input = process.argv;
+if(input[2]=='add'){
+    fs.writeFileSync(input[3],input[4]);
+}
+else if(input[2]=='remove'){
+    fs.unlinkSync(input[3]);
+}
+else
+    console.log("Invalid output");
+
+
+// const dataSet = require('./data.js');
+// const http = require('http');
+// http.createServer((req,rep)=>{
+//     rep.writeHead(200,{'content-type':'application/Json'});
+//     rep.write(JSON.stringify(dataSet));
+//     rep.end();
+
+// }).listen(3000);
 
