322e20ae159b0623cdae2c552aa58ee18a23cb74
Abhijit Maiti <avhijitmaiti0123@gmail.com>
2024-01-06 00:53:10 +0530

    frist commit

---
+++ weather_data.js
@@ -0,0 +1,176 @@
+
+const daysInWeek = ["Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday", "Sunday"];
+
+const weatherData = {
+    Region: "india",
+    Time_zone: "2:02+GMT",
+    Current_time: "10:00",
+    Date: "12 Nov 2023",
+    Day: daysInWeek[2],
+    City: "Moyna",
+    Coordinate: {
+        lat: "2.442112",
+        long: "75.7346",
+    },
+    Temparature: {
+        celcius: "34.5",
+        fharanite: "273",
+    },
+    Humadity: "23",
+    Chance_of_rain: "50%",
+    Wind: {
+        wind_speed1: "30 km/hr",
+        wind_speed2: "q60 m/hr",
+        wind_direction: "south-west",
+    },
+    Weather_forecast_day: {
+        Mon: {
+            temp: "40",
+            wind_speed1: "30 km/hr",
+            wind_speed2: "q60 m/hr",
+            wind_direction: "south-west",
+            humadity: "23",
+            chance_of_rain: "50%",
+
+        },
+        Tue: {
+            temp: "40",
+            wind_speed1: "30 km/hr",
+            wind_speed2: "q60 m/hr",
+            wind_direction: "south-west",
+            humadity: "23",
+            chance_of_rain: "50%",
+
+        }
+        ,
+        Wed: {
+            temp: "40",
+            wind_speed1: "30 km/hr",
+            wind_speed2: "q60 m/hr",
+            wind_direction: "south-west",
+            humadity: "23",
+            chance_of_rain: "50%",
+        },
+        
+        Thu: {
+            temp: "40",
+            wind_speed1: "30 km/hr",
+            wind_speed2: "q60 m/hr",
+            wind_direction: "south-west",
+            humadity: "23",
+            chance_of_rain: "50%",
+
+        },
+        Fri: {
+            temp: "40",
+            wind_speed1: "30 km/hr",
+            wind_speed2: "q60 m/hr",
+            wind_direction: "south-west",
+            humadity: "23",
+            chance_of_rain: "50%",
+
+        },
+        Sat: {
+            temp: "40",
+            wind_speed1: "30 km/hr",
+            wind_speed2: "q60 m/hr",
+            wind_direction: "south-west",
+            humadity: "23",
+            chance_of_rain: "50%",
+
+        },
+        Sun: {
+            temp: "40",
+            wind_speed1: "30 km/hr",
+            wind_speed2: "q60 m/hr",
+            wind_direction: "south-west",
+            humadity: "23",
+            chance_of_rain: "50%",
+
+        },
+
+    },
+    Weather_forecast_time: {
+        current_time: "10:30",
+
+        "10:30": {
+            temp: "40",
+            wind_speed1: "30 km/hr",
+            wind_speed2: "q60 m/hr",
+            wind_direction: "south-west",
+            humadity: "23",
+            chance_of_rain: "50%",
+
+        },
+        "11:00": {
+            temp: "40",
+            wind_speed1: "30 km/hr",
+            wind_speed2: "q60 m/hr",
+            wind_direction: "south-west",
+            humadity: "23",
+            chance_of_rain: "50%",
+
+        },
+        "11:30": {
+            temp: "40",
+            wind_speed1: "30 km/hr",
+            wind_speed2: "q60 m/hr",
+            wind_direction: "south-west",
+            humadity: "23",
+            chance_of_rain: "50%",
+
+        },
+        "12:00": {
+            temp: "40",
+            wind_speed1: "30 km/hr",
+            wind_speed2: "q60 m/hr",
+            wind_direction: "south-west",
+            humadity: "23",
+            chance_of_rain: "50%",
+
+        },
+        "12:30": {
+            temp: "40",
+            wind_speed1: "30 km/hr",
+            wind_speed2: "q60 m/hr",
+            wind_direction: "south-west",
+            humadity: "23",
+            chance_of_rain: "50%",
+
+        },
+        "1:00": {
+            temp: "40",
+            wind_speed1: "30 km/hr",
+            wind_speed2: "q60 m/hr",
+            wind_direction: "south-west",
+            humadity: "23",
+            chance_of_rain: "50%",
+
+        },
+        "1:30": {
+            temp: "40",
+            wind_speed1: "30 km/hr",
+            wind_speed2: "q60 m/hr",
+            wind_direction: "south-west",
+            humadity: "23",
+            chance_of_rain: "50%",
+
+        },
+        "2:00": {
+            temp: "40",
+            wind_speed1: "30 km/hr",
+            wind_speed2: "q60 m/hr",
+            wind_direction: "south-west",
+            humadity: "23",
+            chance_of_rain: "50%",
+
+        }
+
+
+
+
+
+
+    }
+}
+module.exports=weatherData;
